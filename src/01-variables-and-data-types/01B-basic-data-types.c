1655ad5d2943c588dcfbfb36d936c3b4f897fe50
awpala <adam.w.pala@gmail.com>
2020-12-18 23:30:11 -0600

    Add basic data types lesson

---
+++ src/01-variables-and-data-types/01B-basic-data-types.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h> // C99 - provides bool type alias and literals true and false
+
+int main()
+{
+    float jason = 23.333;
+    double jason2 = 55.5555555555555e+11;
+    _Bool boolVariable = 1; // C89
+    bool myBoolean = true; // C99
+
+    return 0;
+}
