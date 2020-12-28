ac3dffdb625fc2a119740c962f2f89e235361ca6
awpala <adam.w.pala@gmail.com>
2020-12-28 09:35:30 -0600

    Add switch-case lesson

---
+++ src/03-control-flow/03B-switch.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    enum Weekday {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
+    enum Weekday today = Monday;
+
+    switch (today)
+    {
+        case Sunday:
+            printf("Today is Sunday.\n");
+            break;
+        case Monday:
+            printf("Today is Monday.\n");
+            break;
+        case Tuesday:
+            printf("Today is Tuesday.\n");
+            break;
+        default:
+            printf("Whatever\n");
+            break;
+    }
+
+    return 0;
+}
