b3a3258c3f854acde04d762cf2050a73bc085029
awpala <adam.w.pala@gmail.com>
2021-01-01 11:46:44 -0600

    Add functions intro lesson

---
+++ src/05-functions/05A-functions-intro.c
@@ -0,0 +1,16 @@
+// function prototype
+void add();
+
+int main()
+{
+    // function invocation/call
+    add();
+
+    return 0;
+}
+
+// function implementation/definition
+void add() // function header
+{
+    // function body
+}
