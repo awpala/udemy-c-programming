b67f26067e8000a0f049c04315f8e9cdb2c0463e
awpala <adam.w.pala@gmail.com>
2021-01-02 19:51:36 -0600

    Add using dynamic memory challenge

---
+++ src/07-pointers/07L-challenge-using-dynamic-memory.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int size;
+    char* text = NULL;
+
+    // From the terminal, run as: ./07L-using-dynamic-memory
+    printf("Enter limit of the text: ");
+    scanf("%d", &size);
+
+    text = (char*)malloc(size * sizeof(char));
+
+    if (text != NULL) {
+        printf("\nEnter some text: ");
+        scanf(" ");
+        gets(text);
+
+        printf("\nInputted text is: \"%s\"\n\n", text);
+    }
+
+    free(text);
+    text = NULL;
+
+    return 0;
+}
