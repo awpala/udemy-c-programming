cd7e096a1b0612aa2a763301c1140bbf6089b66f
awpala <adam.w.pala@gmail.com>
2021-01-03 13:32:44 -0600

    Add read file lesson

---
+++ src/09-file-IO/09A-read-file.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *fp;
+
+    // read file character-wise
+    int c;
+
+    fp = fopen("file.txt", "r");
+
+    if (fp == NULL) {
+        perror("Error in opening file");
+        return -1;
+    }
+
+    while ((c = fgetc(fp)) != EOF) {
+        printf("%c", c);
+    }
+    printf("\n\n");
+
+    fclose(fp);
+    fp = NULL;
+
+    // read file by string
+    char str[60];
+
+    fp = fopen("file.txt", "r");
+
+    if (fp == NULL) {
+        perror("Error in opening file");
+        return -1;
+    }
+
+    if (fgets(str, 60, fp) != NULL) { // N.B. reading by line is more efficient than char-by-char
+        printf("%s", str);
+    }
+
+    fclose(fp);
+    fp = NULL;
+
+    return 0;
+}
