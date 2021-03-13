822c7ed0f90dd5303ccbeac863baf1b1304de02d
awpala <adam.w.pala@gmail.com>
2021-03-13 11:43:32 -0600

    Add write file lesson

---
+++ src/09-file-IO/09B-write-file.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  FILE *fp = NULL;
+
+  // fputc()
+  fp = fopen("09B-file-fputc.txt", "w+");
+  int ch;
+  for (ch = 33; ch <=100; ch++) {
+    fputc(ch, fp);
+  }
+  fclose(fp);
+  fp = NULL;
+  
+  // fputs()
+  fp = fopen("09B-file-fputs.txt", "w+");
+  fputs("This is a C course.\n", fp);
+  fputs("I am happy to be here", fp);
+  fclose(fp);
+  fp = NULL;
+
+  // fprintf()
+  fp = fopen("09B-file-fprintf.txt", "w+");
+  if (!fp)
+    fprintf(fp, "%s %s %s %s %d", "Hello", "my", "number", "is", 555);
+  fclose(fp);
+  fp = NULL;
+
+  return 0;
+}
